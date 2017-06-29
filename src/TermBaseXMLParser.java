685970e238b953c72f00d0f25d160e97f2d740e2
YmraNaissur <daily.spb@yandex.ru>
2017-06-29 18:17:08 +0300

    Обошел косяк с перепутанными местами оригинальным и целевым языками

--- src/TermBaseXMLParser.java
+++ src/TermBaseXMLParser.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 
 public class TermBaseXMLParser {
-    private int termSize = 4; // максимальное количество терминов одного языка
+    private int termSize = 5; // максимальное количество терминов одного языка
     private int notesSize = 0; // записываем сюда нужно кол-во столбцов под примечания
     private String sourceLanguage; // исходный язык
     private String targetLanguage; // целевой язык
@@ -58,24 +58,41 @@ public class TermBaseXMLParser {
         Entry entry = new Entry();
 
         // извлекаем из XML-ки все теги <conceptgrp>
-        List<Element> elements = doc.getElementsByTag("conceptgrp");
+        List<Element> elements = doc.getElementsByTag("conceptGrp");
 
         for (Element e: elements) {
             // теперь из каждого conceptgrp выделяем все элементы languageGrp
             List<Element> languageGroups = e.getElementsByTag("languageGrp");
 
                 // Элементов languageGrp получается два (для оригинального и целевого языков).
+                // Но почему-то бывает, что иногда первым в XML идет оригинальный, язык, а следующим - целевой,
+                // а иногда наоборот
 
-                // Поэтому сначала ищем все элементы term в первом languageGrp, это будут термины оригинального языка
+                // Проверяем все элементы term в первом languageGrp
                 List<Element> terms = languageGroups.get(0).getElementsByTag("term");
-                for (Element term: terms) {
-                    entry.originalTerms.add(term.text());
-                }
+                // Определяем, являются ли эти термины терминами оригинального языка
+                if (languageGroups.get(0).getElementsByAttributeValue("lang", sourceLanguage).size() > 0) {
+                    // Если да, значит заносим их сразу в список оригинальных терминов
+                    for (Element term: terms) {
+                        entry.originalTerms.add(term.text());
+                    }
 
-                // Затем ищем все элементы term во втором languageGrp (целевые термины)
-                terms = languageGroups.get(1).getElementsByTag("term");
-                for (Element term: terms) {
-                    entry.targerTerms.add(term.text());
+                    // Затем ищем все элементы term во втором languageGrp (целевые термины)
+                    terms = languageGroups.get(1).getElementsByTag("term");
+                    for (Element term: terms) {
+                        entry.targerTerms.add(term.text());
+                    }
+                } else {
+                    // Если нет, значит это целевой язык и мы сначала заносим эти термины в список targetTerms
+                    for (Element term: terms) {
+                        entry.targerTerms.add(term.text());
+                    }
+
+                    // А затем ищем все элементы во втором languageGrp, они будут оригинальными
+                    terms = languageGroups.get(1).getElementsByTag("term");
+                    for (Element term: terms) {
+                        entry.originalTerms.add(term.text());
+                    }
                 }
 
                 // Извлекаем все примечания
